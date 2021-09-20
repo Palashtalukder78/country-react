53d052769968c33bd9015a2b791100467bd63436
Palashtalukder78 <palashtalukder78@gmail.com>
2021-09-20 19:41:44 +0600

    setup environment for that js dude

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Countries from './component/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
+      
     </div>
   );
 }
